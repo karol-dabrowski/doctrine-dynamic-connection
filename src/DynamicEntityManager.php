9d9209268ac78f35d114f694a4679519e259890a
Karol Dabrowski <kontakt@karoldabrowski.pl>
2021-02-12 15:49:32 +0100

    Create a decorator for entity manager and configure autoload

---
+++ src/DynamicEntityManager.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace DynamicConnection;
+
+use Doctrine\ORM\Decorator\EntityManagerDecorator;
+
+final class DynamicEntityManager extends EntityManagerDecorator
+{
+
+}
