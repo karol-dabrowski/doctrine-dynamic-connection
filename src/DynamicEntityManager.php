006e4e0ed900510c0eefa2f1b543cdbf4d1da385
Karol Dabrowski <kontakt@karoldabrowski.pl>
2021-02-12 18:17:58 +0100

    Implement changeDatabase method in DynamicEntityManager class

--- src/DynamicEntityManager.php
+++ src/DynamicEntityManager.php
@@ -4,8 +4,18 @@ declare(strict_types=1);
 namespace DynamicConnection;
 
 use Doctrine\ORM\Decorator\EntityManagerDecorator;
+use DynamicConnection\Exception\ConnectionTypeException;
 
 final class DynamicEntityManager extends EntityManagerDecorator
 {
+	public function changeDatabase(string $databaseName): void
+	{
+		$connection = $this->getConnection();
+		if(!$connection instanceof DynamicConnection) {
+			throw new ConnectionTypeException();
+		}
 
+		$this->clear();
+		$connection->changeDatabase($databaseName);
+	}
 }
