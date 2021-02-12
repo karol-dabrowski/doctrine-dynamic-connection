f800c74389b2ab9e002164e0badf23578bb8c61c
Karol Dabrowski <kontakt@karoldabrowski.pl>
2021-02-12 18:04:55 +0100

    Create custom exception for wrong connection type

---
+++ src/Exception/ConnectionTypeException.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace DynamicConnection\Exception;
+
+use DynamicConnection\DynamicConnection;
+use Exception;
+
+class ConnectionTypeException extends Exception
+{
+	public function __construct()
+	{
+		parent::__construct(sprintf(
+			'Wrong connection type. Instance of %s expected.', DynamicConnection::class
+		));
+	}
+}
