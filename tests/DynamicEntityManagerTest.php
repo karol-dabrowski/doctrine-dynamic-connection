2274cbbd071af317f23619a585dedb10d7e98d84
Karol Dabrowski <kontakt@karoldabrowski.pl>
2021-02-16 22:23:35 +0100

    Create basic unit test for DynamicEntityManager class

---
+++ tests/DynamicEntityManagerTest.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests;
+
+use Doctrine\ORM\EntityManager;
+use DynamicConnection\DynamicEntityManager;
+use PHPUnit\Framework\TestCase;
+
+class DynamicEntityManagerTest extends TestCase
+{
+	public function testCanBeCreated()
+	{
+		$entityManagerMock = $this->createMock(EntityManager::class);
+		$dynamicEntityManager = new DynamicEntityManager($entityManagerMock);
+
+		$this->assertInstanceOf(DynamicEntityManager::class, $dynamicEntityManager);
+	}
+}
