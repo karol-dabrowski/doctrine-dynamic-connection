8f84d5f5cf7cb9656f432a9df82f524e06ba6b6a
Karol Dabrowski <kontakt@karoldabrowski.pl>
2021-02-20 17:51:27 +0100

    Add arguments for username, password, host and port  in changeDatabase method

--- src/DynamicEntityManager.php
+++ src/DynamicEntityManager.php
@@ -9,8 +9,13 @@ use DynamicConnection\Exception\ConnectionTypeException;
 
 final class DynamicEntityManager extends EntityManagerDecorator
 {
-    public function changeDatabase(string $databaseName): void
-    {
+    public function changeDatabase(
+        string $databaseName,
+        string $username = null,
+        string $password = null,
+        string $host = null,
+        int $port = null
+    ): void {
         $connection = $this->getConnection();
         if (!$connection instanceof DynamicConnection) {
             throw new ConnectionTypeException();
@@ -24,6 +29,22 @@ final class DynamicEntityManager extends EntityManagerDecorator
 
         $params = [];
         $params['dbName'] = $databaseName;
+
+        if ($username) {
+            $params['user'] = $username;
+        }
+
+        if ($password) {
+            $params['password'] = $password;
+        }
+
+        if ($host) {
+            $params['host'] = $host;
+        }
+
+        if ($port) {
+            $params['port'] = $port;
+        }
 
         $connection->reinitialize($params);
     }
