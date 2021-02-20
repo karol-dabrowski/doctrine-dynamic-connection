f887ccc2f367362ee763f06b57e30638a5a6bf07
Karol Dabrowski <kontakt@karoldabrowski.pl>
2021-02-20 15:01:19 +0100

    Add unit test checking if correct exception is thrown when connection class is wrong

--- tests/DynamicEntityManagerTest.php
+++ tests/DynamicEntityManagerTest.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use Doctrine\DBAL\Connection;
 use Doctrine\ORM\EntityManager;
+use DynamicConnection\DynamicConnectionWrapper;
 use DynamicConnection\DynamicEntityManager;
+use DynamicConnection\Exception\ConnectionTypeException;
 use PHPUnit\Framework\TestCase;
 
 class DynamicEntityManagerTest extends TestCase
@@ -16,5 +19,21 @@ class DynamicEntityManagerTest extends TestCase
         $dynamicEntityManager = new DynamicEntityManager($entityManagerMock);
 
         $this->assertInstanceOf(DynamicEntityManager::class, $dynamicEntityManager);
+    }
+
+    public function testThrowsExceptionWhenConnectionTypeIsWrong()
+    {
+        $connectionMock = $this->createMock(Connection::class);
+
+        $entityManagerMock = $this->createMock(EntityManager::class);
+        $entityManagerMock->method('getConnection')->willReturn($connectionMock);
+
+        $dynamicEntityManager = new DynamicEntityManager($entityManagerMock);
+
+        $this->assertInstanceOf(Connection::class, $dynamicEntityManager->getConnection());
+        $this->assertNotInstanceOf(DynamicConnectionWrapper::class, $dynamicEntityManager->getConnection());
+
+        $this->expectException(ConnectionTypeException::class);
+        $dynamicEntityManager->changeDatabase('testDb');
     }
 }
